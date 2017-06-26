586160eff5be35bc6931d2326e1b31ac3c12e68a
Roctober92 <wolfe.andrew@gmail.com>
2017-06-26 15:13:59 -0300

    Lolzers
    Lolzers

---
+++ Sandbox/starwars.js
@@ -0,0 +1,16 @@
+//helper function to fetch the data from an external source
+function getJSON(url) {
+    return fetch(url)
+        .then(function (response) {
+        return response.json();
+    })
+        .catch(function (error) {
+        console.log(error);
+    });
+}
+
+
+
+
+
+
