59b2f1f57c8af9e3d2a205e26387fb5b1ebf99d7
Roctober92 <wolfe.andrew@gmail.com>
2017-06-28 13:00:25 -0300

    Vocab
    Vocab

--- Sandbox/starwars.js
+++ Sandbox/starwars.js
@@ -11,6 +11,52 @@ function getJSON(url) {
 
 
 
+//lets get some ships
+function fetchShips() {
+    var url = "//swapi.co/api/starships/";
+    getJSON(url).then(function (data) {
+
+        console.log(data);
+        var results = data.results
+        // data.results[0].name
+        // results[0].name
+
+        //get list element where id="shipList
+        var shiplistElement = document.getElementById('shipList')
+
+        // make sure HTML is nuked
+        shiplistElement.innerHTML = "";
+
+        // for every ship in 'results' list
+        results.forEach(function(ship) {
+
+            var list = document.createElement('li');
+            var anchor = document.createElement('a');
+
+
+            // set href attribute to url of ship
+            // ship.url because in forEach, separates
+            // results into ship
+            anchor.setAttribute('href', ship.url)
+
+
+            // set link element contents to name of ship
+            //  CreateTextNode = ship.name would work
+            anchor.innerHTML = ship.name
+
+
+            // add the link to the list item
+            // list item to list
+            list.appendChild(anchor)
+            shiplistElement.appendChild(list)
+        });
+    });
+}
+
+
+
+fetchShips();
+
 
 
 
