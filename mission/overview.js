6599a06ad76cb034ad210ef7dadcfb0e44472af6
Roctober92 <wolfe.andrew@gmail.com>
2017-07-25 20:43:32 -0300

    Fixed Area Link
    Stuff

--- mission/overview.js
+++ mission/overview.js
@@ -2,7 +2,7 @@ var elements = document.getElementsByTagName("a");
 for (var i = 0; i < elements.length; i++) {
     if (elements[i].className == 'mtc') {
         elements[i].onclick = function () {
-            document.body.style.backgroundImage = "url('directory/eden.jpg')";
+            document.body.style.backgroundImage = "url('directory/eden.JPG')";
         };
     };
     if (elements[i].className == 'merlo') {
@@ -12,7 +12,7 @@ for (var i = 0; i < elements.length; i++) {
     };
     if (elements[i].className == 'arco') {
         elements[i].onclick = function () {
-            document.body.style.backgroundImage = "url('directory/arco.jpg')";
+            document.body.style.backgroundImage = "url('directory/arco.JPG')";
         };
     };
     if (elements[i].className == 'sl') {
@@ -22,22 +22,22 @@ for (var i = 0; i < elements.length; i++) {
     };
     if (elements[i].className == 'itu') {
         elements[i].onclick = function () {
-            document.body.style.backgroundImage = "url('directory/ituz.jpg')";
+            document.body.style.backgroundImage = "url('directory/ituz.JPG')";
         };
     };
     if (elements[i].className == 'lujan') {
         elements[i].onclick = function () {
-            document.body.style.backgroundImage = "url('directory/apostasia.jpg')";
+            document.body.style.backgroundImage = "url('directory/apostasia.JPG')";
         };
     };
     if (elements[i].className == 'psm') {
         elements[i].onclick = function () {
-            document.body.style.backgroundImage = "url('directory/altos.jpg')";
+            document.body.style.backgroundImage = "url('directory/altos.JPG')";
         };
     };
     if (elements[i].className == 'chivilcoy') {
         elements[i].onclick = function () {
-            document.body.style.backgroundImage = "url('directory/club.jpg')"
+            document.body.style.backgroundImage = "url('directory/club.JPG')"
         };
     };
 };
