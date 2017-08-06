8f16438be9ad0438464f722045aa9c5ca9d58c9b
Roctober92 <wolfe.andrew@gmail.com>
2017-08-06 09:47:18 -0300

    Overview
    background images

--- mission/overview.js
+++ mission/overview.js
@@ -37,7 +37,77 @@ for (var i = 0; i < elements.length; i++) {
     };
     if (elements[i].className == 'chivilcoy') {
         elements[i].onclick = function () {
-            document.body.style.backgroundImage = "url('directory/club.JPG')"
+            document.body.style.backgroundImage = "url('directory/club.JPG')";
         };
     };
+};
+
+var caro = document.getElementById('caro');
+caro.onclick = function () {
+    document.body.style.backgroundImage = "url('directory/carobap.JPG')";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundAttachment = "fixed";
+};
+
+var bar = document.getElementById('barrowes');
+bar.onclick = function () {
+    document.body.style.backgroundImage = "url('directory/barrowes.jpg')";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundAttachment = "fixed";
+};
+
+var os = document.getElementById('oscar');
+os.onclick = function () {
+    document.body.style.backgroundImage = "url('directory/oscarbap.jpg')";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundAttachment = "fixed";
+};
+
+var m = document.getElementById('mtc');
+m.onclick = function () {
+    document.body.style.backgroundImage = "url('directory/mtc.jpg')";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundAttachment = "fixed";
+};
+
+var d = document.getElementById('dad');
+d.onclick = function () {
+    document.body.style.backgroundImage = "url('directory/dad.jpg')";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundAttachment = "fixed";
+};
+
+var mar = document.getElementById('maria');
+mar.onclick = function () {
+    document.body.style.backgroundImage = "url('directory/maria.jpg')";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundAttachment = "fixed";
+};
+
+var rom = document.getElementById('rom');
+rom.onclick = function () {
+    document.body.style.backgroundImage = "url('directory/rom.jpg')";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundAttachment = "fixed";
+};
+
+var hector = document.getElementById('hector');
+hector.onclick = function () {
+    document.body.style.backgroundImage = "url('directory/oscar.jpg')";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundAttachment = "fixed";
+};
+
+var da = document.getElementById('david');
+da.onclick = function () {
+    document.body.style.backgroundImage = "url('directory/david.jpg')";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundAttachment = "fixed";
+};
+
+var colt = document.getElementById('colt');
+colt.onclick = function () {
+    document.body.style.backgroundImage = "url('directory/colton.jpg')";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundAttachment = "fixed";
 };
